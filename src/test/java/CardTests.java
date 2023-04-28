324e5e1c3fa7260b042cc6db2991b8d0abcd62f8
Beloglazova Olga <beloglazovaom@yandex.ru>
2023-04-28 18:20:15 +0300

    update project: add tests, one failed

---
+++ src/test/java/CardTests.java
@@ -0,0 +1,46 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class CardTests {
+
+    @BeforeEach
+    void shouldOpenBrowser() {
+        open("http://localhost:7777/");
+    }
+
+
+    @Test
+    public void shouldSendForm() {
+        $("[type=text]").setValue( "Иван Иванов" );
+        $("[type=tel]").setValue( "+79261234567");
+        $(".checkbox__box").click();
+        $(".button__content").click();
+        $("[data-test-id=order-success]").shouldHave( exactText(" Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.") );
+
+    }
+
+
+    @Test
+    public void shouldSendFormWithDashInName() {
+        $("[type=text]").setValue( "Иван Иванов-Петров" );
+        $("[type=tel]").setValue( "+79261234567");
+        $(".checkbox__box").click();
+        $(".button__content").click();
+        $("[data-test-id=order-success]").shouldHave( exactText(" Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.") );
+
+    }
+
+    @Test
+    public void shouldSendFormWithLitter() {
+        $("[type=text]").setValue( "Алёна Савина" );
+        $("[type=tel]").setValue( "+79261234567");
+        $(".checkbox__box").click();
+        $(".button__content").click();
+        $("[data-test-id=order-success]").shouldHave( exactText(" Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.") );
+
+    }
+
+}
