d6087583f8cda7728a40c77ff41a437135bd8568
Beloglazova Olga <beloglazovaom@yandex.ru>
2023-04-29 05:39:45 +0300

    port=7777 yml

--- src/test/java/CardTests.java
+++ src/test/java/CardTests.java
@@ -2,7 +2,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+
 
 public class CardTests {
 
